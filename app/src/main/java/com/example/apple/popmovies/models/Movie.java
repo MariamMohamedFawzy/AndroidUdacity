b39623740563c03e79e657e020df89d3d51632e3
Apple <apple@Apples-MacBook-Pro-3.local>
2016-01-12 08:31:46 +0200

    make movie Parcelable instead of Serializable

--- app/src/main/java/com/example/apple/popmovies/models/Movie.java
+++ app/src/main/java/com/example/apple/popmovies/models/Movie.java
@@ -1,15 +1,17 @@
 
 package com.example.apple.popmovies.models;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
-import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
 
-public class Movie implements Serializable {
+public class Movie implements Parcelable {
 
     @SerializedName("poster_path")
     @Expose
@@ -53,6 +55,36 @@ public class Movie implements Serializable {
     @SerializedName("vote_average")
     @Expose
     private Double voteAverage;
+
+    public Movie(Parcel in) {
+        id  = in.readInt();
+        posterPath = in.readString();
+        overview = in.readString();
+        releaseDate = in.readString();
+        originalTitle = in.readString();
+        originalLanguage = in.readString();
+        title = in.readString();
+        backdropPath = in.readString();
+        voteAverage = in.readDouble();
+        voteCount = in.readInt();
+        video = Boolean.valueOf(in.readString());
+    }
+
+    public static final Creator<Movie> CREATOR = new Creator<Movie>() {
+        @Override
+        public Movie createFromParcel(Parcel in) {
+            return new Movie(in);
+        }
+
+        @Override
+        public Movie[] newArray(int size) {
+            return new Movie[size];
+        }
+    };
+
+    public Movie() {
+
+    }
 
     /**
      * 
@@ -306,4 +338,23 @@ public class Movie implements Serializable {
         this.voteAverage = voteAverage;
     }
 
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeInt(id);
+        parcel.writeString(posterPath);
+        parcel.writeString(overview);
+        parcel.writeString(releaseDate);
+        parcel.writeString(originalTitle);
+        parcel.writeString(originalLanguage);
+        parcel.writeString(title);
+        parcel.writeString(backdropPath);
+        parcel.writeInt(voteCount);
+        parcel.writeDouble(voteAverage);
+        parcel.writeString(video.toString());
+    }
 }
